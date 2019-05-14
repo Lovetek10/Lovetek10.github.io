484020a9ed39b4eb027e3fc4443ae7e976f46b0c
Lovetek10 <sam@lovetek.co.uk>
2019-05-14 15:49:42 +0100

    Updates

---
+++ precache-manifest.3d40ae1bf6f34c099dc270e0d8ec471e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d55d6a2cdab8aaecb0e33b5a60eb8d69",
+    "url": "/lovetek10.github.io/index.html"
+  },
+  {
+    "revision": "800b74f577e4cc419fe7",
+    "url": "/lovetek10.github.io/static/css/main.6f7b816c.chunk.css"
+  },
+  {
+    "revision": "f679e7d05fab56d424cb",
+    "url": "/lovetek10.github.io/static/js/2.74ed4d15.chunk.js"
+  },
+  {
+    "revision": "800b74f577e4cc419fe7",
+    "url": "/lovetek10.github.io/static/js/main.e06cb917.chunk.js"
+  },
+  {
+    "revision": "16ae29e85549352ec570",
+    "url": "/lovetek10.github.io/static/js/runtime~main.4a3641ca.js"
+  },
+  {
+    "revision": "f2d57d1b3c34de12844921fb749caeb3",
+    "url": "/lovetek10.github.io/static/media/1.f2d57d1b.png"
+  },
+  {
+    "revision": "7800c0feabcaed71f3cffbf9ad68722a",
+    "url": "/lovetek10.github.io/static/media/2.7800c0fe.png"
+  },
+  {
+    "revision": "51ddb3867b149b3fe56bff140a721397",
+    "url": "/lovetek10.github.io/static/media/3.51ddb386.png"
+  },
+  {
+    "revision": "0f88ca298d175d76369b5fac1287afdf",
+    "url": "/lovetek10.github.io/static/media/4.0f88ca29.png"
+  }
+]);
